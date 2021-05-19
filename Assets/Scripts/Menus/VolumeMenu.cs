80641e8fc75c8d8bdc59f0984f6ab2f840a4bbb3
Maja Hedegard <mh223pi@student.lnu.se>
2021-05-19 11:20:37 +0200

    add audioMixer

--- Assets/Scripts/Menus/VolumeMenu.cs
+++ Assets/Scripts/Menus/VolumeMenu.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class VolumeMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
     public void SetVolume(float volume)
     {
-        Debug.Log(volume);
+        audioMixer.SetFloat("Volume", volume);
     }
 }
